e5e5a1afa824977f1febdef0bc7ef809b7876560
Yousuf <josephali1993@gmail.com>
2022-01-11 21:59:34 +0400

    First commit

---
+++ App.js
@@ -0,0 +1,68 @@
+
+import React from 'react';
+import type { Node } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Pressable } from 'react-native';
+
+//these imports are important for translation !
+
+import { useState } from 'react';
+import './assets/i18n/i18n';
+import { useTranslation } from 'react-i18next';
+
+
+const App: () => Node = () => {
+
+  const { t, i18n } = useTranslation();
+
+  const [currentLanguage, setLanguage] = useState('en');
+
+  const changeLanguage = value => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch(err => console.log(err));
+  };
+
+  return (
+ 
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+        }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 25, color: '#33A850' }}>
+          {t('title')}{' '}
+        </Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 25, color: '#33A850' }}>
+          {t('message')}
+        </Text>
+        <Pressable
+          onPress={() => changeLanguage('en')}
+          style={{
+            backgroundColor:
+              currentLanguage === 'en' ? '#33A850' : '#d3d3d3',
+            padding: 20,
+          }}>
+          <Text>Select English</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => changeLanguage('ar')}
+          style={{
+            backgroundColor:
+              currentLanguage === 'ar' ? '#33A850' : '#d3d3d3',
+            padding: 20,
+          }}>
+          <Text>للغة العربية</Text>
+        </Pressable>
+      </View>
+ 
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ assets/i18n/readme.txt
@@ -0,0 +1,6 @@
+Project Referances:|
+https://www.geeksforgeeks.org/multi-language-support-in-react-native/
+
+when downloading from git install
+npm install i18next --save
+npm i react-i18next
